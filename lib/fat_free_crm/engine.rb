5ff62146fef3ad90ed662706fb758525fb5bc4ce
Nathan Broadbent <nathan.f77@gmail.com>
2011-08-04 15:53:09 +0800

    Workaround to support plugin loading from within an engine

--- lib/fat_free_crm/engine.rb
+++ lib/fat_free_crm/engine.rb
@@ -5,6 +5,16 @@ module FatFreeCrm
     initializer "static assets" do |app|
       app.middleware.insert_before(::ActionDispatch::Static, ::ActionDispatch::Static, "#{root}/public")
     end
+
+    # Load our packaged plugins
+    config.before_initialize do
+      Rails.application.config.paths.vendor.plugins.push File.expand_path('../../../vendor/plugins', __FILE__)
+      Dir.glob(File.expand_path('../../../vendor/plugins', __FILE__) + "/*").each do |plugin_path|
+        # Add each plugin's lib folder to the path, then run init.rb
+        $: << File.join(plugin_path, 'lib')
+        require File.join(plugin_path, 'init.rb')
+      end
+    end
   end
 end
 
