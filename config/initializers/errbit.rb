db14d860caec1bc74a54c09556afe5d8f5a58d0e
Nathan Broadbent <nathan.f77@gmail.com>
2012-05-28 18:38:27 +1200

    'Airbrake enabled!' message

--- config/initializers/errbit.rb
+++ config/initializers/errbit.rb
@@ -1,6 +1,10 @@
-Airbrake.configure do |config|
-  config.api_key = ENV['ERRBIT_API_KEY']
-  config.host    = ENV['ERRBIT_HOST']
-  config.port    = 80
-  config.secure  = config.port == 443
+if ENV['ERRBIT_API_KEY']
+  Airbrake.configure do |config|
+    config.api_key = ENV['ERRBIT_API_KEY']
+    config.host    = ENV['ERRBIT_HOST']
+    config.port    = ENV['ERRBIT_PORT']
+    config.secure  = config.port == 443
+  end
+
+  puts "Airbrake enabled!"
 end
