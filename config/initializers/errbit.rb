500b8807154c17672355b5f3c89a23daf75f9b69
Nathan Broadbent <nathan.f77@gmail.com>
2012-05-30 11:06:42 +1200

    Notify errbit for development exceptions if ENV['ERRBIT_NOTIFY_IN_DEVELOPMENT']

--- config/initializers/errbit.rb
+++ config/initializers/errbit.rb
@@ -4,6 +4,8 @@ if ENV['ERRBIT_API_KEY']
     config.host    = ENV['ERRBIT_HOST']
     config.port    = ENV['ERRBIT_PORT']
     config.secure  = config.port == 443
+    # Send errors to Errbit in development if ENV variable is set	
+    config.development_environments = [] if ENV['ERRBIT_NOTIFY_IN_DEVELOPMENT']
   end
 
   puts "Airbrake enabled!"
